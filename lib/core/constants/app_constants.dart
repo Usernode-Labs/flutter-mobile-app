7a938bacd1fb343f759711d8f83a06a494132507
lingash25 <snaitmouloud@gmail.com>
2025-10-09 09:59:34 +0200

    Change the wording of the splash screen

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,8 @@
 class AppConstants {
   // App Information
   static const String appName = 'Usernode';
-  static const String appTagline = 'Your Gateway to DeFi';
+  static const String appTagline =
+      'A New Layer 1. Operated & Secured by You. From Your Phone';
 
   // Animation Durations
   static const Duration splashDuration = Duration(seconds: 3);
